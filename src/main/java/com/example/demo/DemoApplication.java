99ddb65463dde08ddc3d8456edf3a0a5376ebea1
cy <390518881@qq.com>
2019-10-12 10:31:00 +0800

    提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @ComponentScan("com")
 @PropertySource(value = "dev.properties")
+@ImportResource("classpath:transaction.xml")
 public class DemoApplication {
 
     public static void main(String[] args) {
