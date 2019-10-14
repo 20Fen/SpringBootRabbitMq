2dceab571ad5d6236e1142501b4b78301efcc112
cy <390518881@qq.com>
2019-10-14 16:56:58 +0800

    提交

--- src/main/java/com/example/demo/system/model/po/image.java
+++ src/main/java/com/example/demo/system/model/po/image.java
@@ -2,19 +2,11 @@ package com.example.demo.system.model.po;
 
 import lombok.Data;
 
-import java.util.List;
-
 /**
- * Description:
- *
- * @author yangfl
- * @date 2019年10月14日 14:11
- * Version 1.0
+ * Description:  文件路径类
  */
 @Data
 public class image {
-
-
 
     private String id;
     private String url;
