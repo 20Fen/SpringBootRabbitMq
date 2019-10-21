e34435dd702e465e83693b8d7451cd1a58d135a3
cy <390518881@qq.com>
2019-10-21 11:17:41 +0800

    提交

--- src/main/java/com/example/demo/system/model/po/TestPo.java
+++ src/main/java/com/example/demo/system/model/po/TestPo.java
@@ -2,7 +2,11 @@ package com.example.demo.system.model.po;
 
 import com.example.demo.system.model.bo.BaseProtocolIn;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Pattern;
 
 /**
  * Description: 数据库model
@@ -12,7 +16,11 @@ import lombok.Data;
 public class TestPo extends BaseProtocolIn {
 
 //    @NotBlank(message = "id不能为空")
-    private String id;
+//    @ApiModelProperty(value = "振幅危险阈值(kPa)")
+//    @Length(max = 16, message = "最大只能输入16个字符")
+//    @Pattern(regexp = "^[0-9]*[1-9][0-9]*$", message = "page只允许是正整数")
+
+private String id;
     private String planNo;
     private String endTime;
     private String statTime;
