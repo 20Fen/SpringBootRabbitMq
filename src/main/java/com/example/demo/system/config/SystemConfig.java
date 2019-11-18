1c9deccdc49c2d344e41f42bad9921d5d5ee73dc
cy <390518881@qq.com>
2019-11-18 14:25:01 +0800

    提交

--- src/main/java/com/example/demo/system/config/SystemConfig.java
+++ src/main/java/com/example/demo/system/config/SystemConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
  * 描述: 系统配置(获取properties文件配置)
  */
 @Configuration
+//file:${user.dir}得到项目的目录 ${runtime.conf.active}配置的环境变量
 @PropertySource(value = "file:${user.dir}/conf/${runtime.conf.active}.properties", encoding = "utf-8")
 public class SystemConfig {
 
