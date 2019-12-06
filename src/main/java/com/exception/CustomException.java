3740448608e9ae9928c649fbc3ddbda1d7d58985
cy <15622969639@163.com>
2019-12-06 14:09:28 +0800

    提交

--- src/main/java/com/exception/CustomException.java
+++ src/main/java/com/exception/CustomException.java
@@ -3,7 +3,7 @@ package com.exception;
 /**
  * Description: 自定义异常类
  */
-public class CustomException extends Exception {
+public class CustomException extends RuntimeException {
     private static final long	serialVersionUID	= 12L;
     private int					errorCode;
     private String				message;
