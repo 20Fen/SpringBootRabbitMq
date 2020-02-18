c4e1d2c893e6dcbfed1cce12108c73f840ce2584
yankewei <y15622969639@163.com>
2020-02-18 14:39:10 +0800

    Y

--- src/main/java/com/example/demo/system/model/po/City.java
+++ src/main/java/com/example/demo/system/model/po/City.java
@@ -1,5 +1,6 @@
 package com.example.demo.system.model.po;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -9,6 +10,7 @@ import java.io.Serializable;
  * Description: 省级区域
  */
 @Data
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class City implements Serializable {
 
     private String id;
